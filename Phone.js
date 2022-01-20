189a8252eef43d2dbab6a4e3eeb8236f3c1a4f33
Jimmy-36 <89508072+Jimmy-36@users.noreply.github.com>
2022-01-20 17:38:57 -0600

    Update

---
+++ Phone.js
@@ -0,0 +1,24 @@
+/**
+ * Creating classes:
+ *
+ * Class declaration: class Name {}
+ * Class expression:  const Name = class {}
+ */
+
+class Phone {
+  constructor(
+    // Defines parameters:
+    color,
+    length,
+    width,
+    
+  ) {
+    // Define properties:
+
+    this.color = color;
+    this.length = length;
+    this.width = width;
+  }
+}
+
+export default Phone;
